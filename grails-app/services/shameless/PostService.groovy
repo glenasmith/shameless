5cb5f603e0786b90d80c84379cac506b9ed14510
Glen Smith <glen@bytecode.com.au>
2011-10-14 17:02:56 +1100

    Added service with exception handling

--- grails-app/services/shameless/PostService.groovy
+++ grails-app/services/shameless/PostService.groovy
@@ -9,4 +9,26 @@ class PostService {
         return "glen"
 
     }
+
+    Post createPost(String username, String status, boolean badFood = false, long longitude = 0, long latitude = 0) {
+        Account account = Account.findByUsername(username)
+        if (account) {
+            Post newPost = new Post(account: account, status: status, badFood: badFood, longitude: longitude, latitude: latitude)
+            if (newPost.save()) {
+                return newPost
+            } else {
+                throw new PostException(message: "Invalid post parameter", post: newPost)
+            }
+        } else {
+            throw new PostException(message: "Could not locate account")
+        }
+
+    }
+}
+
+class PostException extends RuntimeException {
+
+    String message
+    Post post
+
 }
