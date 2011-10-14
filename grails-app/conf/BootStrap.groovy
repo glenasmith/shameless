02ec9f5f733414343060949fe677ef04cf2c073a
Glen Smith <glen@bytecode.com.au>
2011-10-14 14:51:23 +1100

    Bootstrap tidies for demo

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,23 +8,16 @@ class BootStrap {
 
     def init = { servletContext ->
 
-
-//        if (GrailsUtil.environment == "test") {
-//              def testUser = new Account(username: 'admin', realName: "Test Admin", password: 'test', enabled: true)
-//              testUser.save(failOnError: true)
-//              springSecurityService.currentUser = testUser;
-//        }
-
-
-
     	if (Account.count() == 0) {
-//    		def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-//      		def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+//    		def adminRole = new Role(authority: 'ROLE_ADMIN')
+//          assert adminRole.save(flush: true)
+//      	def userRole = new Role(authority: 'ROLE_USER')
+//          assert userRole.save(flush: true)
 //
-//      		def adminUser = new Account(username: 'admin', enabled: true, password: 'admin123', realName: 'Admin User')
-//      		adminUser.save(flush: true)
+//      	def adminUser = new Account(username: 'admin', enabled: true, password: 'admin123', realName: 'Admin User')
+//      	assert adminUser.save(flush: true)
 //
-//      		AccountRole.create adminUser, adminRole, true
+//      	AccountRole.create adminUser, adminRole, true
 
             fixtureLoader.load("accounts")
 
@@ -37,6 +30,11 @@ class BootStrap {
 //        assert new PricingPlan(name: 'Basic', dollarsPerMonth: 0.0)
 //        assert new PricingPlan(name: 'Premium', dollarsPerMonth: 10.0)
 //        assert new PricingPlan(name: 'Enterprise', dollarsPerMonth: 10000.0)
+        environments {
+            test {
+                // Test specific bootstrapping of users
+            }
+        }
     	
     }
     def destroy = {
