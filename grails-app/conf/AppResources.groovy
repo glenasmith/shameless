37a04ae8b46a3df13d4e7e51d5597673ec8d39e4
Glen Smith <glen@bytecode.com.au>
2011-10-21 14:26:28 +1100

    Updates jqplot resources

--- grails-app/conf/AppResources.groovy
+++ grails-app/conf/AppResources.groovy
@@ -1,73 +1,60 @@
 modules = {
 
-	less {
-        resource url:'/js/less-1.1.3.min.js'
+    less {
+        resource url: '/js/less-1.1.3.min.js'
     }
 
     seefooddiet {
-        resource url:'/less/seefooddiet.less', attrs: [type: 'css', rel: 'stylesheet/less']
+        resource url: '/less/seefooddiet.less', attrs: [type: 'css', rel: 'stylesheet/less']
     }
-    
+
     twitter {
-    	resource url: '/css/bootstrap.min.css'
+        resource url: '/css/bootstrap.min.css'
     }
 
     twitterLess {
         //dependsOn 'less'
-        resource url:'/less/bootstrap.less', attrs: [type: 'css', rel: 'stylesheet/less']
-        
+        resource url: '/less/bootstrap.less', attrs: [type: 'css', rel: 'stylesheet/less']
+
     }
 
     jqueryValidate {
         dependsOn 'jquery'
-        resource url:'/js/jquery.validate.js'
+        resource url: '/js/jquery.validate.js'
     }
+
 
     jqPlot {
         dependsOn 'jquery'
-        resource url:'/css/jquery.jqplot.css'
-        resource url:'/js/jquery.jqplot.js'
+
+        resource url: '/css/jquery.jqplot.css'
+        resource url: "/js/jquery.jqplot.js"
+        resource url: "/js/jqplot-plugins/jqplot.barRenderer.min.js"
+        resource url: "/js/jqplot-plugins/jqplot.categoryAxisRenderer.min.js"
+        resource url: "/js/jqplot-plugins/jqplot.pointLabels.min.js"
+
+
     }
 
     backbone {
-    	dependsOn 'jquery'
-    	
-    	resource url:'/js/json2.js' // for old browsers
-    	resource url:'/js/underscore-min.js'
-    	resource url:'/js/backbone.js'
-    	//resource url:'/js/mustache.js'
-    	resource url:'/js/handlebars.1.0.0.beta.3.js'
+        dependsOn 'jquery'
+
+        resource url: '/js/json2.js' // for old browsers
+        resource url: '/js/underscore-min.js'
+        resource url: '/js/backbone.js'
+        //resource url:'/js/mustache.js'
+        resource url: '/js/handlebars.1.0.0.beta.3.js'
     }
-    
+
     dashboard {
-    	dependsOn 'backbone,jquery-ui'
-    	resource url:'/js/dashboard.js'
+        dependsOn 'backbone,jquery-ui'
+        resource url: '/js/dashboard.js'
     }
 
     backboneDashboard {
-    	dependsOn 'backbone,jquery-ui'
-    	resource url:'/js/backbone-dashboard.js'
+        dependsOn 'backbone,jquery-ui'
+        resource url: '/js/backbone-dashboard.js'
     }
 
-     /*
-
-    twitterSupport {
-        dependsOn 'less'
-
-		resource url:'/less/reset.less', attrs: [type: 'css', rel: 'stylesheet/less']
-		resource url:'/less/variables.less', attrs: [type: 'css', rel: 'stylesheet/less']
-		resource url:'/less/mixins.less', attrs: [type: 'css', rel: 'stylesheet/less']
-
-		resource url:'/less/scaffolding.less', attrs: [type: 'css', rel: 'stylesheet/less']
-
-        resource url:'/less/type.less', attrs: [type: 'css', rel: 'stylesheet/less']
-        resource url:'/less/forms.less', attrs: [type: 'css', rel: 'stylesheet/less']
-        resource url:'/less/tables.less', attrs: [type: 'css', rel: 'stylesheet/less']
-        resource url:'/less/patterns.less', attrs: [type: 'css', rel: 'stylesheet/less']
-
-		resource url:'/less/application.less', attrs: [type: 'css', rel: 'stylesheet/less']
-
-    }
-    */
 
 }
