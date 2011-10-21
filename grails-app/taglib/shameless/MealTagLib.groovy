e0d4676668481a951ca3d9704f1d29300e8f955e
Glen Smith <glen@bytecode.com.au>
2011-10-21 11:20:23 +1100

    Updated meal taglib

--- grails-app/taglib/shameless/MealTagLib.groovy
+++ grails-app/taglib/shameless/MealTagLib.groovy
@@ -2,36 +2,43 @@ package shameless
 
 class MealTagLib {
 
-    def postTable = { attrs ->
 
-        if (attrs.posts) {
+    def mealList = { attrs ->
+
+        if (attrs.meals) {
+
             out << '''
 <table id="postlist">
 <thead><tr>  <th>Date</th> <th>Meal</th> <th>Bad Food?</th> </tr></thead>
 '''
-            attrs.posts.each { post ->
+            attrs.meals.each { meal ->
                 out << """
-<tr>
-      <td> ${post.dateCreated}  </td>
-      <td> ${post.status}  </td>
-"""
-                out << (post.badFood ? '<td style="bad">BAD BAD BAD</td>' : '<td>Great choice!</td>')
+      <tr>
+                      <td> ${meal.dateCreated}  </td>
+                      <td> ${meal.status}  </td>
+                  """
+                out << (meal.badFood ? ' <td style="bad">BAD BAD BAD</td> ' :
+                          '<td>Great choice!</td>')
+                out << '</tr>'
+
             }
+            out << '</table>'
+
+        } else {
+            out << "  Sorry, no posts available right now"
+        }
+        out
+    }
+
+
+    def mealListRender = { attrs ->
+         if (attrs.meals) {
+            out << render(template: '/meal/mealTable', model:[meals: attrs.meals] )
         } else {
             out << "Sorry, no posts available right now"
         }
         out
-
     }
 
-
-    def postTableRender = { attrs ->
-         if (attrs.posts) {
-            out << render(template: '/meal/mealTable', model:[posts: attrs.posts] )
-        } else {
-            out << "Sorry, no posts available right now"
-        }
-        out
-    }
 
 }
