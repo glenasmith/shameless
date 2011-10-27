ab5cad113bcdfb123c72eb86f52092fe313b7342
Glen Smith <glen@bytecode.com.au>
2011-10-27 07:44:08 -0500

    Commented out tag lib spec for demo

--- test/unit/shameless/MealTagLibSpec.groovy
+++ test/unit/shameless/MealTagLibSpec.groovy
@@ -5,50 +5,52 @@ import spock.lang.Ignore
 
 class MealTagLibSpec extends TagLibSpec {
 
-    def "ensure populated display table is working"() {
-      given:
-      def someMeals = [
-              new Meal(dateCreated: new Date(), status: "Pizza Again", badFood: true),
-              new Meal(dateCreated: new Date(), status: "Fruit Salad", badFood: false),
-              new Meal(dateCreated: new Date(), status: "Nachos", badFood: true)
-              ]
 
-      when:
-      def output = mealList(meals : someMeals)
-
-      then:
-      (output =~ /style="bad"/).count == 2
-      (output =~ /Great choice!/).count == 1
-
-    }
-
-    def "ensure empty display table is working"() {
-
-      when:
-      def output = mealList(meals: [])
-
-      then:
-      output =~ /Sorry, no posts available right now/
-
-    }
-
-     @Ignore
-     def "ensure our refactored render tag is working"() {
-      given:
-      def someMeals = [
-              new Meal(dateCreated: new Date(), status: "Pizza Again", badFood: true),
-              new Meal(dateCreated: new Date(), status: "Fruit Salad", badFood: false),
-              new Meal(dateCreated: new Date(), status: "Nachos", badFood: true)
-              ]
-
-      when:
-      def output = mealListRender(meals : someMeals)
-
-      then:
-      (output =~ /style="bad"/).count == 2
-      (output =~ /Great choice!/).count == 1
-
-    }
 
 
 }
+
+//   def "ensure populated display table is working"() {
+//      given:
+//      def someMeals = [
+//              new Meal(dateCreated: new Date(), status: "Pizza Again", badFood: true),
+//              new Meal(dateCreated: new Date(), status: "Fruit Salad", badFood: false),
+//              new Meal(dateCreated: new Date(), status: "Nachos", badFood: true)
+//              ]
+//
+//      when:
+//      def output = mealList(meals : someMeals)
+//
+//      then:
+//      (output =~ /style="bad"/).count == 2
+//      (output =~ /Great choice!/).count == 1
+//
+//    }
+//
+//    def "ensure empty display table is working"() {
+//
+//      when:
+//      def output = mealList(meals: [])
+//
+//      then:
+//      output =~ /Sorry, no posts available right now/
+//
+//    }
+//
+//     @Ignore
+//     def "ensure our refactored render tag is working"() {
+//      given:
+//      def someMeals = [
+//              new Meal(dateCreated: new Date(), status: "Pizza Again", badFood: true),
+//              new Meal(dateCreated: new Date(), status: "Fruit Salad", badFood: false),
+//              new Meal(dateCreated: new Date(), status: "Nachos", badFood: true)
+//              ]
+//
+//      when:
+//      def output = mealListRender(meals : someMeals)
+//
+//      then:
+//      (output =~ /style="bad"/).count == 2
+//      (output =~ /Great choice!/).count == 1
+//
+//    }
